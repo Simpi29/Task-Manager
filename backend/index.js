af33618dd43200eee0d916a1bc517a4853f3098e
Simpi.12222147 <sd623099@gmail.com>
2024-04-14 17:19:06 +0530

    chl ja

--- backend/index.js
+++ backend/index.js
@@ -6,18 +6,10 @@ const path = require("path");
 const auth = require("./routes/auth");
 const list = require("./routes/list");
 app.use(express.json());
-app.use(cors({
-  origin: ["https://deploy-mern-1whq.vercel.app"],
-  methods: ["POST", "GET", "PUT", "PATCH", "DELETE"], // Include PUT, PATCH, and DELETE methods
-  credentials: true
-}));
+app.use(cors(
+  ));
 
-mongoose
-.connect(
- // "mongodb+srv://Simpi:Sd2771825@@cluster0.r0ovj38.mongodb.net/"
-  "mongodb+srv://simpi:simpi@cluster0.nbpp3nj.mongodb.net/"
- 
-)
+
 app.use("/api/v1", auth);
 app.use("/api/v2", list);
 
